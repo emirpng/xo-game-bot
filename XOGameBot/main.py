340790e5e6dc580f10c3b15061f0d974c6b9b242
Emir <marveldarkbull@gmail.com>
2022-12-25 02:14:44 +0300

    Update main.py

--- XOGameBot/main.py
+++ XOGameBot/main.py
@@ -7,9 +7,9 @@ from pyrogram.types import InlineQuery, InlineQueryResultArticle, InputTextMessa
 
 
 app = Client(session_name="XOGame",
-             api_id=os.environ.get("API_ID"),
-             api_hash=os.environ.get("API_HASH"),
-             bot_token=os.environ.get("BOT_TOKEN")
+             api_id=os.environ.get("9248715"),
+             api_hash=os.environ.get("a9c1288681c2d3265175ff96c619d064"),
+             bot_token=os.environ.get("5841942016:AAEUeDRIkS7IdlKKnNWHBme6T1GfKHQGeSM")
              )
 
 
